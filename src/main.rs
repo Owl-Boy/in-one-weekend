4b063c09f684767ab22437b4b1702c094d1a6ae6
Owl-Boy <sharmashubh223@gmail.com>
2022-11-05 00:56:07 +0530

    rendered a sphere... sort of

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,23 @@ const VIEWPORT_HEIGHT: f32 = 2.0;
 const VIEWPORT_WIDTH: f32 = VIEWPORT_HEIGHT * ASPECT_RATIO;
 const FOCAL_LENGTH: f32 = 1.0;
 
+fn hit_sphere(center: Point, radius: f32, r: &Ray) -> bool {
+    let oc = r.origin - center;
+    let a = Vec3::dot(r.dir, r.dir);
+    let b = 2.0 * Vec3::dot(oc, r.dir);
+    let c = Vec3::dot(oc, oc) - radius * radius;
+    let discr = b*b - 4.0*a*c;
+    discr > 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
-    let unit_direction = r.dir.unit_along();
-    let t = (unit_direction.y + 1.0) * 0.5;
-    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+    if hit_sphere(Point::new(0.0, 0.0, -1.0), 0.5, r) {
+        Color::new(1.0, 0.0, 0.0)
+    } else {
+        let unit_direction = r.dir.unit_along();
+        let t = (unit_direction.y + 1.0) * 0.5;
+        Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
+    }
 }
 
 fn main() {
@@ -28,17 +41,13 @@ fn main() {
     let origin = Point::new(0.0, 0.0, 0.0);
     let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
     let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
-    let lower_left_corner = (origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH));
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
 
     // Render
     println!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255");
 
     for j in (0..IMAGE_HEIGHT).rev() {
         for i in 0..IMAGE_WIDTH {
-            // let r = (i as f32)/((IMAGE_WIDTH - 1) as f32);
-            // let g = (j as f32)/((IMAGE_HEIGHT - 1) as f32);
-            // let b = 0.25;
-            // let pixel_color = Color::new(r, g, b);
             let u = (i as f32)/ ((IMAGE_WIDTH - 1) as f32);
             let v = (j as f32)/ ((IMAGE_HEIGHT - 1) as f32);
             let r = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v - origin);
